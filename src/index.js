96f1e31207661761c8608ca318b83aa681422310
Jayden <contact@jbcov.com>
2023-05-11 20:13:12 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default {
+	async fetch(request, env, ctx) {
+		return new Response("Hello World!");
+		https://api.apify.com/v2/actor-tasks/demonstrative_eel~get-all-winery-reviews/runs
+	},
+};
