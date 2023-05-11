dbae2817fc52290ae72f2a24842d324ad4be5c94
Jayden <contact@jbcov.com>
2023-05-11 21:48:59 +1000

    run apify actor function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import runApifyActor from "./runApifyActor";
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-		https://api.apify.com/v2/actor-tasks/demonstrative_eel~get-all-winery-reviews/runs
-	},
+  async fetch(request, env, ctx) {
+    runApifyActor();
+  },
 };
